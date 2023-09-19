41f83b20977f415eb7566537566c59909d6d0864
Andrew Kleindolph <akleindolph@gmail.com>
2023-09-19 15:12:31 -0700

    Update tft_lcd_large_text.py

--- tft_lcd_large_text.py
+++ tft_lcd_large_text.py
@@ -4,18 +4,29 @@
 CircuitPython simple text display demo
 """
 import board
+import time
 import terminalio
 from adafruit_display_text import bitmap_label
 
-# Update this to change the text displayed.
-text = "Hello, World!"
-# Update this to change the size of the text displayed. Must be a whole number.
-scale = 3
-
-text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=scale)
-text_area.x = 10
-text_area.y = 10
-board.DISPLAY.show(text_area)
+red = 0xFF0000
+purple = 0xFF00FF
 
 while True:
-    pass
+    # Update this to change the text displayed.
+    text = "Hello, toads!"
+    # Update this to change the size of the text displayed. Must be a whole number.
+    scale = 3
+
+    text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=scale, color=red)
+    text_area.x = 10
+    text_area.y = 50
+    board.DISPLAY.show(text_area)
+
+    time.sleep (0.5)
+    
+    text = "Hello, frogs!"
+    text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=scale, color=purple)
+    text_area.x = 10
+    text_area.y = 50
+    board.DISPLAY.show(text_area)
+    time.sleep (0.5)
