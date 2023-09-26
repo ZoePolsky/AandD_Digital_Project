115b2e480b39e55c56431069293de6d4a36494f2
Andrew Kleindolph <akleindolph@gmail.com>
2023-09-25 19:06:55 -0700

    Create large_distance.py

---
+++ large_distance.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Carter Nelson for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+# Simple demo of the VL53L1X distance sensor.
+# Will print the sensed range/distance every second.
+
+import time
+import board
+import adafruit_vl53l1x
+import terminalio
+from adafruit_display_text import bitmap_label
+
+greenish = 0x00FF99
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+vl53 = adafruit_vl53l1x.VL53L1X(i2c)
+
+# OPTIONAL: can set non-default values
+vl53.distance_mode = 1
+vl53.timing_budget = 100
+
+print("VL53L1X Simple Test.")
+print("--------------------")
+model_id, module_type, mask_rev = vl53.model_info
+print("Model ID: 0x{:0X}".format(model_id))
+print("Module Type: 0x{:0X}".format(module_type))
+print("Mask Revision: 0x{:0X}".format(mask_rev))
+print("Distance Mode: ", end="")
+if vl53.distance_mode == 1:
+    print("SHORT")
+elif vl53.distance_mode == 2:
+    print("LONG")
+else:
+    print("UNKNOWN")
+print("Timing Budget: {}".format(vl53.timing_budget))
+print("--------------------")
+
+vl53.start_ranging()
+
+while True:
+    if vl53.data_ready:
+        print("Distance: {} cm".format(vl53.distance))
+        vl53.clear_interrupt()
+
+    text = str(vl53.distance) +" cm"
+    # Update this to change the size of the text displayed. Must be a whole number.
+    scale = 5
+
+    text_area = bitmap_label.Label(terminalio.FONT, text=text, scale=scale, color=greenish)
+    text_area.x = 5
+    text_area.y = 50
+    board.DISPLAY.show(text_area)
+
+    time.sleep (0.1)
